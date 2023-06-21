f698309a5473f2c1fb2cbc96e4d9b0f9c27d58a4
TiagoFreitas03 <tiago.freitas.faria@gmail.com>
2023-06-20 22:53:06 -0300

    remoção de imports

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,8 +1,6 @@
 import { ButtonHTMLAttributes } from "react"
 import clsx from 'clsx'
 
-import { NUMBERS, OPERATIONS } from '../utils/keys'
-
 interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
 	text: string
 	icon?: string
@@ -12,8 +10,8 @@ export function Button({ text, icon, ...rest }: ButtonProps) {
 	return (
 		<button
 			className={clsx("w-14 h-14 m-2 text-lg", {
-				"text-gray-200": NUMBERS.includes(text) || text === '=',
-				"text-blue-500": !NUMBERS.includes(text) && text !== '=',
+				"text-gray-200": !isNaN(Number(text)) || text === '=',
+				"text-blue-500": isNaN(Number(text)) && text !== '=',
 				"bg-blue-500 rounded-full text-zinc-900": text === '=',
 				"hover:bg-zinc-900": text !== '='
 			})}
