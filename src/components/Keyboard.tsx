108c507ceff043e5436b92a3b2310dde676d7dae
TiagoFreitas03 <tiago.freitas.faria@gmail.com>
2023-06-24 18:40:31 -0300

    separação dos botões da calculadora em outro arquivo

--- src/components/Keyboard.tsx
+++ src/components/Keyboard.tsx
@@ -1,59 +1,21 @@
 import { Button } from "./Button"
+import { BUTTONS } from '../constants/BUTTONS'
 
 interface KeyboardProps {
 	onKeyClick: (key: string) => void
 }
 
-const BUTTONS = [
-	[
-		{ key: 'C', icon: 'c' },
-		{ key: 'Backspace', icon: 'delete-left' },
-		{ key: '%', icon: 'percent' },
-		{ key: '/', icon: 'divide' },
-	],
-	[
-		{ key: '7', icon: '7' },
-		{ key: '8', icon: '8' },
-		{ key: '9', icon: '9' },
-		{ key: '*', icon: 'xmark' },
-	],
-	[
-		{ key: '4', icon: '4' },
-		{ key: '5', icon: '5' },
-		{ key: '6', icon: '6' },
-		{ key: '-', icon: 'minus' },
-	],
-	[
-		{ key: '1', icon: '1' },
-		{ key: '2', icon: '2' },
-		{ key: '3', icon: '3' },
-		{ key: '+', icon: 'plus' },
-	],
-	[
-		{ key: 'r', icon: 'plus-minus' },
-		{ key: '0', icon: '0' },
-		{ key: '.' },
-		{ key: 'Enter', icon: 'equals' }
-	]
-]
-
 export function Keyboard({ onKeyClick }: KeyboardProps) {
 	return (
 		<div>
 			{
-				BUTTONS.map(row => (
-					<div key={row.map(r => r.key).join('-')}>
-						{
-							row.map(({ key, icon }) => (
-								<Button
-									key={key}
-									onClick={() => onKeyClick(key)}
-									text={key}
-									icon={icon}
-								/>
-							))
-						}
-					</div>
+				BUTTONS.map(({ key, icon, text }) => (
+					<Button
+						key={key}
+						onClick={() => onKeyClick(key)}
+						text={text ?? key}
+						icon={icon}
+					/>
 				))
 			}
 		</div>
---
+++ src/constants/BUTTONS.ts
@@ -0,0 +1,28 @@
+interface Button {
+	key: string
+	icon?: string
+	text?: string
+}
+
+export const BUTTONS: Button[] = [
+	{ key: 'Escape', icon: 'c' },
+	{ key: 'Delete', text: 'CE' },
+	{ key: '%', icon: 'percent' },
+	{ key: '/', icon: 'divide' },
+	{ key: '7', icon: '7' },
+	{ key: '8', icon: '8' },
+	{ key: '9', icon: '9' },
+	{ key: '*', icon: 'xmark' },
+	{ key: '4', icon: '4' },
+	{ key: '5', icon: '5' },
+	{ key: '6', icon: '6' },
+	{ key: '-', icon: 'minus' },
+	{ key: '1', icon: '1' },
+	{ key: '2', icon: '2' },
+	{ key: '3', icon: '3' },
+	{ key: '+', icon: 'plus' },
+	{ key: '+/-', icon: 'plus-minus' },
+	{ key: '0', icon: '0' },
+	{ key: '.', text: ',' },
+	{ key: 'Enter', icon: 'equals' }
+]
