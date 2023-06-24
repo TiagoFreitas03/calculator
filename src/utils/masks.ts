0a0a77b1501c1cbc210594eb891fcfdb5b95b13c
TiagoFreitas03 <tiago.freitas.faria@gmail.com>
2023-06-24 00:21:45 -0300

    máscaras para entrada e a expressão numérica

---
+++ src/utils/masks.ts
@@ -0,0 +1,52 @@
+import { onlyNumbers } from "./only-numbers"
+
+function maskExpression(expression: string[]) {
+	for (let i = 0; i < expression.length; i++) {
+		if (!isNaN(Number(expression[i]))) {
+			expression[i] = maskNumber(expression[i])
+		}
+	}
+
+	return expression
+}
+
+function maskNumber(str: string, parenthesis: boolean = false) {
+	if (str.replace('.', '').length !== onlyNumbers(str).length) {
+		return str
+	}
+
+	const decimalSeparatorPos = str.indexOf('.')
+	let integerPart = str
+	let decimalPart = ''
+
+	if (decimalSeparatorPos > 0) {
+		integerPart = str.split('.')[0]
+		decimalPart = str.split('.')[1]
+	}
+
+	const num: string[] = []
+	const digits = integerPart.split('')
+	let i = 0
+
+	for (let pos = digits.length - 1; pos >= 0; pos--) {
+		num.unshift(digits[pos])
+		i++
+
+		if (i % 3 === 0 && i < digits.length) {
+			num.unshift('.')
+		}
+	}
+
+	if (decimalSeparatorPos > 0) {
+		num.push(`,${decimalPart}`)
+	}
+
+	if (Number(num.join('')) < 0 && parenthesis) {
+		num.unshift('(')
+		num.push(')')
+	}
+
+	return num.join('')
+}
+
+export { maskNumber, maskExpression }
