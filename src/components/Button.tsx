b530327369151c54a913edb5d5b4708674386ce6
TiagoFreitas03 <tiago.freitas.faria@gmail.com>
2023-06-24 18:39:45 -0300

    adicionando a classe font-bold no button

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -9,7 +9,7 @@ interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
 export function Button({ text, icon, ...rest }: ButtonProps) {
 	return (
 		<button
-			className={clsx("w-14 h-14 m-2 text-lg outline-none", {
+			className={clsx("w-14 h-14 m-2 text-lg outline-none font-bold", {
 				"text-gray-200": !isNaN(Number(text)) || text === 'Enter',
 				"text-blue-500": isNaN(Number(text)) && text !== 'Enter',
 				"bg-blue-500 rounded-full text-zinc-900": text === 'Enter',
