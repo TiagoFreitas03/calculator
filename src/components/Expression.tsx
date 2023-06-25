288dd0e08d8ce9d49baeb43e5d984d83fd31fd21
TiagoFreitas03 <tiago.freitas.faria@gmail.com>
2023-06-25 19:59:09 -0300

    ajuste na exibição do ícone na expressão

--- src/components/Expression.tsx
+++ src/components/Expression.tsx
@@ -2,6 +2,7 @@ import clsx from "clsx"
 
 import { OPERATIONS } from "../constants/KEYS"
 import { maskExpression } from "../utils/masks"
+import { OperatorIcon } from "./OperatorIcon"
 
 interface ExpressionProps {
 	value: string[]
@@ -21,15 +22,10 @@ export function Expression({ value }: ExpressionProps) {
 			{
 				maskedExpression.map((element, index) => {
 					return [...OPERATIONS, '='].includes(element) ?
-						<i
+						<OperatorIcon
+							operator={element}
+							cssClasses='text-sm ml-2 text-blue-300'
 							key={element + index}
-							className={clsx('fas text-sm ml-2 text-blue-300', {
-								'fa-plus': element === '+',
-								'fa-minus': element === '-',
-								'fa-xmark': element === '*',
-								'fa-divide': element === '/',
-								'fa-equals': element === '='
-							})}
 						/> :
 						<span key={element + index} className="ml-2 text-zinc-400">
 							{element}
