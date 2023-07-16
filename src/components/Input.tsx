1e3c489b496010e3cd21e3799a3675bde8bd5239
TiagoFreitas03 <tiago.freitas.faria@gmail.com>
2023-07-16 18:44:24 -0300

    alteração para input grande

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,16 +1,21 @@
+import clsx from "clsx"
 import { InputHTMLAttributes } from "react"
 
 interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
 	label: string
+	big?: boolean
 }
 
-export function Input({ label, ...rest }: InputProps) {
+export function Input({ label, big = false, ...rest }: InputProps) {
 	return (
 		<>
 			<label>{ label }</label>
 
 			<input
-				className="flex-1 ml-4 bg-black text-right outline-none text-blue-300"
+				className={clsx("bg-black text-right outline-none text-blue-300", {
+					'w-full text-3xl': big,
+					'flex-1': !big
+				})}
 				{...rest}
 			/>
 		</>
