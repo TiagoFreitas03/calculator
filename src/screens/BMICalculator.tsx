a2f77aa5ebcf24a2b8a9abd3bbf332b7fd86a0b2
TiagoFreitas03 <tiago.freitas.faria@gmail.com>
2023-07-17 21:48:52 -0300

    alteração no cálculo de IMC

--- src/screens/BMICalculator.tsx
+++ src/screens/BMICalculator.tsx
@@ -1,33 +1,52 @@
 import { FormEvent, useState } from "react"
 
-import { Input } from "../components/Input"
 import { Button } from '../components/Button'
 import { BMIResult } from "../components/BMIResult"
+import { Entry } from "../components/Entry"
+import { Keyboard } from "../components/Keyboard"
 
 export function BMICalculator() {
 	const [weight, setWeight] = useState('60')
 	const [height, setHeight] = useState('170')
-	const [bmi, setBmi] = useState(calculateBMI())
+	const [activeParam, setActiveParam] = useState<'w' | 'h'>('w')
+	const [bmi, setBmi] = useState<number>()
+	const [clearWeight, setClearWeight] = useState(true)
+	const [clearHeight, setClearHeight] = useState(true)
 
 	function validateParam(value: string) {
-		const num = Number(value)
+		return !(isNaN(Number(value)) || !(/^([0-9]{1,3})(\.[0-9]{0,2})?$/).test(value))
+	}
 
-		if (value === '' || isNaN(num) || !(/^([0-9]{1,3})(\.[0-9]{0,2})?$/).test(value)) {
-			return false
+	function handleParamChange(value: string) {
+		if (validateParam(value)) {
+			if (activeParam === 'w') {
+				setWeight(value)
+				setClearWeight(false)
+			}
+			else {
+				setHeight(value)
+				setClearHeight(false)
+			}
 		}
+	}
 
-		return num > 0
+	function handleParamClick(param: 'w' | 'h') {
+		setBmi(undefined)
+		setActiveParam(param)
 	}
 
 	function handleCalculateFormSubmit(e: FormEvent) {
 		e.preventDefault()
+		const [w, h] = [Number(weight), Number(height)];
 
-		if (!validateParam(weight) || !validateParam(height)) {
+		if (!validateParam(weight) || !validateParam(height) || w <= 0 || h <= 0) {
 			alert('Parâmetro(s) inválido(s)')
 			return
 		}
 
 		setBmi(calculateBMI())
+		setClearWeight(true)
+		setClearHeight(true)
 	}
 
 	function calculateBMI() {
@@ -40,36 +59,50 @@ export function BMICalculator() {
 		<>
 			<form onSubmit={handleCalculateFormSubmit}>
 				<div className="pb-1 border-b border-zinc-800">
-					<Input
-						autoFocus
-						label="Peso"
-						type="number"
-						step={0.01}
-						value={weight}
-						onChange={e => setWeight(e.target.value)}
-						big
+					<span className="absolute">Peso</span>
+
+					<Entry
+						clear={clearWeight}
+						text={weight}
+						active={activeParam === 'w'}
+						onTextChange={text => handleParamChange(text)}
+						onClick={() => handleParamClick('w')}
+						cssClasses={activeParam === 'w' ? 'text-blue-300' : ''}
 					/>
 
-					<span className="text-right text-sm">Quilogramas</span>
+					<span className="text-right text-sm -mt-2">Quilogramas</span>
 				</div>
 
-				<div className="mt-3 pb-1 border-b border-zinc-800">
-					<Input
-						label="Altura"
-						type="number"
-						step={0.01}
-						value={height}
-						onChange={e => setHeight(e.target.value)}
-						big
+				<div className="mt-4 pb-1 border-b border-zinc-800">
+					<span className="absolute">Altura</span>
+
+					<Entry
+						clear={clearHeight}
+						text={height}
+						active={activeParam === 'h'}
+						onTextChange={text => handleParamChange(text)}
+						onClick={() => handleParamClick('h')}
+						cssClasses={activeParam === 'h' ? 'text-blue-300' : ''}
 					/>
 
-					<span className="text-right text-sm">Centímetros</span>
+					<span className="text-right text-sm -mt-2">Centímetros</span>
 				</div>
 
 				<Button title="Calcular" type="submit" />
 			</form>
 
-			<BMIResult value={bmi} />
+			<div className='w-80 h-[280px] relative mt-2'>
+				{
+					bmi ?
+						<BMIResult value={bmi} /> :
+						<Keyboard
+							layout="COMMON"
+							entry={activeParam === 'w' ? weight : height}
+							onChangeEntry={text => handleParamChange(text)}
+							disabledKeys={['+-']}
+						/>
+				}
+			</div>
 		</>
 	)
 }
