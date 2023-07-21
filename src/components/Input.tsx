0c78d138c4b75c042a09874a0861333c84c016b3
TiagoFreitas03 <tiago.freitas.faria@gmail.com>
2023-07-20 23:15:47 -0300

    remoção da opção "big" do componente Input

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,21 +1,16 @@
-import clsx from "clsx"
 import { InputHTMLAttributes } from "react"
 
 interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
 	label: string
-	big?: boolean
 }
 
-export function Input({ label, big = false, ...rest }: InputProps) {
+export function Input({ label, ...rest }: InputProps) {
 	return (
 		<>
 			<label>{ label }</label>
 
 			<input
-				className={clsx("bg-black text-right outline-none text-blue-300", {
-					'w-full text-3xl': big,
-					'flex-1': !big
-				})}
+				className={"flex-1 bg-black text-right outline-none text-blue-300"}
 				{...rest}
 			/>
 		</>
