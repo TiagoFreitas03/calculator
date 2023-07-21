5ea420f83fe9a1169686e4a4bd0b68ebf69d0b98
TiagoFreitas03 <tiago.freitas.faria@gmail.com>
2023-07-20 23:14:24 -0300

    criação da função onlyHexCharacters

--- src/utils/only-numbers.ts
+++ src/utils/only-numbers.ts
@@ -1,1 +1,5 @@
-export const onlyNumbers = (str: string) => str.replace(/\D/g, '')
+const onlyNumbers = (str: string) => str.replace(/\D/g, '')
+
+const onlyHexCharacters = (str: string) => str.replace(/[^A-Fa-f0-9]/g, '')
+
+export { onlyNumbers, onlyHexCharacters }
