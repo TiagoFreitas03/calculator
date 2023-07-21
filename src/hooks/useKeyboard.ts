266a16e0c25eebdce0571f002513aa76de1a921e
TiagoFreitas03 <tiago.freitas.faria@gmail.com>
2023-07-20 23:16:10 -0300

    ajustes na validação do texto digitado

--- src/hooks/useKeyboard.ts
+++ src/hooks/useKeyboard.ts
@@ -1,8 +1,8 @@
 import { EntryType } from "../types/EntryType"
 import { LETTERS, NUMBERS, TEXT_KEYS } from "../constants/KEYS"
-import { onlyNumbers } from "../utils/only-numbers"
+import { onlyHexCharacters, onlyNumbers } from "../utils/only-numbers"
 
-export function useKeyboard(type: EntryType = 'bin') {
+export function useKeyboard(type: EntryType = 'bin', allowDecimals = true) {
 	function handleKeyPress(text: string, key: string) {
 		if (TEXT_KEYS.includes(key)) {
 			return append(text, key)
@@ -25,14 +25,22 @@ export function useKeyboard(type: EntryType = 'bin') {
 
 	function append(text: string, key: string) {
 		const original = text
+		key = key.toUpperCase()
 
 		if (NUMBERS.includes(key) || LETTERS.includes(key.toLowerCase())) { // 0..9, A..F
-			if (onlyNumbers(text).length < 15) {
+			const conditions = [
+				type === 'bin' && onlyNumbers(text).length < 48,
+				type === 'oct' && onlyNumbers(text).length < 17,
+				type === 'dec' && onlyNumbers(text).length < 15,
+				type === 'hex' && onlyHexCharacters(text).length < 12
+			]
+
+			if (conditions.some(c => c === true)) {
 				text = text === '0' ? key : text + key
 			}
 		}
 		else if (key === '.' && !text.includes(key)) {
-			if (onlyNumbers(text).length < 15) {
+			if (onlyNumbers(text).length < 21) {
 				text = text + key
 			}
 		}
@@ -69,10 +77,13 @@ export function useKeyboard(type: EntryType = 'bin') {
 
 	function validateEntry(text: string) {
 		switch (type) {
-			case 'bin': return (/^\-?[01]*\.?[01]*$/).test(text)
-			case 'oct': return (/^\-?[0-7]*\.?[0-7]*$/).test(text)
-			case 'dec': return (/^\-?[0-9]*\.?[0-9]*$/).test(text)
-			case 'hex': return (/^\-?[0-9A-Fa-f]*\.?[0-9A-Fa-f]*$/).test(text)
+			case 'bin': return (/^[01]+$/g).test(text)
+			case 'oct': return (/^[0-7]+$/g).test(text)
+			case 'dec':
+				return allowDecimals ?
+					(/^\-?[0-9]*\.?[0-9]*$/).test(text) :
+					(/^[0-9]+$/g).test(text)
+			case 'hex': return (/^[0-9A-Fa-f]+$/g).test(text)
 		}
 	}
 
