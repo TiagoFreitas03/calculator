23bf55c810a66db0c9bf29f40458796f7e726aef
TiagoFreitas03 <tiago.freitas.faria@gmail.com>
2024-02-02 00:13:13 -0300

    organização do código

--- src/components/CalculateButton.tsx
+++ src/components/CalculateButton.tsx
@@ -1,9 +1,7 @@
 export function CalculateButton() {
 	return (
 		<button
-			className={
-				"w-full mt-4 bg-blue-400 font-semibold text-zinc-900 py-2 rounded-full outline-none"
-			}
+			className="w-full mt-4 bg-blue-400 font-semibold text-zinc-900 py-2 rounded-full outline-none"
 			type="submit"
 		>
 			Calcular
